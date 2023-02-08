47d58f16e80e56adcd50d190bcbe5eb91aee2364
sonadro <sondre.rooth@gmail.com>
2023-02-08 10:24:56 +0100

    Chapter 3 progress

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 // database connection
 const dbURI = 'mongodb+srv://ostemannen:H1W7i6KdK7P5a-7E1ZKVs1FYBC-5qhw7HlThWac96UwKgsKBtN@node-tutorials.j2nbgno.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-  .then((result) => app.listen(3000))
+  .then((result) => app.listen(80))
   .catch((err) => console.log(err));
 
 // routes
