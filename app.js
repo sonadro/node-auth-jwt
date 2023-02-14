cc801c6ce26d5b14986ce2bfc1b087b5bb7c247f
sonadro <sondre.rooth@gmail.com>
2023-02-14 10:36:52 +0100

    Ep. 9

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
 
 const { dbPass } = require('./config.json');
 
@@ -9,6 +10,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -25,6 +27,23 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, {
+    maxAge: 1000 * 60 * 60 * 24,
+    secure: true
+  });
+
+  res.send('you got the cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+
+});
 
 // auth routes
 app.use(authRoutes);
