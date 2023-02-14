80d56e7ce3bb5ff61a211b04abd163effbcdea92
sonadro <sondre.rooth@gmail.com>
2023-02-14 14:04:44 +0100

    Sending & Receiving cookies

--- app.js
+++ app.js
@@ -35,14 +35,19 @@ app.get('/set-cookies', (req, res) => {
   res.cookie('newUser', false);
   res.cookie('isEmployee', true, {
     maxAge: 1000 * 60 * 60 * 24,
-    secure: true
+    httpOnly: true
   });
 
   res.send('you got the cookies!');
 });
 
 app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
 
+  console.log(cookies);
+  console.log(cookies.newUser);
+
+  res.json(cookies);
 });
 
 // auth routes
